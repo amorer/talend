39947fee03db0863a5521e711ec7d8d707f340dd
amorer <hiparadise@163.com>
2020-12-10 16:50:47 +0800

    Update up2rp.groovy

--- vars/up2rp.groovy
+++ vars/up2rp.groovy
@@ -5,3 +5,22 @@ def copyInfo(message) {
 def sendWarning(message) {
     echo "WARNING: ${message}"
 }
+
+def CopyJunit2Reportportal(zipFileName,studioPath,reportport_Host,reportPortal_Token) {
+     println "...CopyJunit2Reportportal... ${zipFileName}... ${reportport_Host},,,${reportPortal_Token} " 
+     try {
+                            
+                            sh "zip ${zipFileName} ${studioPath}/studio-junit.xml"
+                           //  sh "zip ${zipFileName} ${studioPath}/studio-junit.xml"
+                          // sh "curl -X POST '${reportport_Host}/api/v1/monthly_studio_release/launch/import' -H 'accept: */*' -H 'Content-Type: multipart/form-data' -H 'Authorization: bearer ${reportPortal_Token}' -F file=@${zipFileName}"
+                            // sh "curl -X POST 'http://192.168.17.33:9528/api/v1/monthly_studio_release/launch/import' -H 'accept: */*' -H 'Content-Type: multipart/form-data' -H 'Authorization: bearer ${reportPortal_Token}' -F file=@${zipFileName}"
+                          echo "posted......."
+                        } catch (Exception error) {
+                            //error e.getMessage()
+                            print error.getMessage()
+                            //currentBuild.result = 'FAILURE'
+                        }finally{
+                            echo "finally......."
+                            sh "rm -f ${zipFileName}"
+                        }
+   }
