9762fb6b73021028ef488e8f34635f726b73e5f0
amorer <hiparadise@163.com>
2020-12-26 19:29:39 +0800

    Update getUser.groovy

--- vars/getUser.groovy
+++ vars/getUser.groovy
@@ -1,6 +1,5 @@
 #!/usr/bin/env groovy
-def call(String prompt1 = 'Please enter your data', String prompt2 = 'Please 
-enter your data') {
+def call(String prompt1 = 'Please enter your data', String prompt2 = 'Please enter your data') {
    def resp = input message: '<message>', parameters: [string(defaultValue: '', 
 description: prompt1, name: 'RESPONSE1'), string(defaultValue: '', description: 
 prompt2, name: 'RESPONSE2')]
