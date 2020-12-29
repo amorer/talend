f9eb3955e7c8857baf350ac7c1945c22dee45530
amorer <hiparadise@163.com>
2020-12-29 17:31:04 +0800

    Update timedCommand.groovy

--- vars/timedCommand.groovy
+++ vars/timedCommand.groovy
@@ -1,17 +1,12 @@
-def setCommand(commandToRun) {
-   cmd = commandToRun
+
+@Grab('org.apache.commons:commons-lang3:3.4+')
+import org.apache.commons.lang.time.StopWatch
+def call(String cmdToRun) {
+   def sw = new StopWatch()
+   def proc = "$cmdToRun".execute()
+   sw.start()
+   proc.waitFor()
+   sw.stop()
+   println( "The process took ${(sw.getTime()/1000).toString()} seconds.\n")
 }
 
-def getCommand() {
-   cmd
-}
-
-def runCommand() {
-   timestamps {
-      cmdOut = sh (script:"${cmd}", returnStdout:true).trim()
-   }
-}
-
-def getOutput() {
-   cmdOut
-}
